b55d7c0f22c0528f2727618d5cd6fbe75fa85a0a
Calin Gabriel <calindotgabriel18@gmail.com>
2020-08-05 18:42:26 +0300

    elastic card layout

--- app/jobag-web/src/routes/home/index.js
+++ app/jobag-web/src/routes/home/index.js
@@ -2,19 +2,27 @@ import { h, Component } from 'preact';
 import Card from 'preact-material-components/Card';
 import 'preact-material-components/Card/style.css';
 import 'preact-material-components/Button/style.css';
-import style from './style';
+import cls from './style';
 
 import results from '../../results';
 
 export default class Home extends Component {
 	render() {
 		return (
-			<div class={`${style.home} page`}>
+			<div class={`${cls.home} page`}>
 				<h1>Jobs</h1>
 				<Card>
-					<div class={style.cardBody}>
-                        {results.map(j => {
-                            return <Card>{j.title}</Card>;
+					<div class={cls.containerCard}>
+                        {results.reverse().map(j => {
+                            return <Card class={cls.card}>
+                                <div class={cls.cardHeader}>
+                                    {j.title}
+                                </div>
+                                <div class={cls.cardBody}>
+                                    <p>{j.city}</p>
+                                    <p>{j.date}</p>
+                                </div>
+                            </Card>;
                         })}
 					</div>
 				</Card>
